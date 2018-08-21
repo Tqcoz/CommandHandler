785fa9af677c6cf2e0a9718eff985127901e91b8
Tqcoz <33169104+Tqcoz@users.noreply.github.com>
2018-08-21 13:20:41 -0400

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -2,12 +2,12 @@ const pkg = require('../package.json')
 
 module.exports.run = (bot, message, args) => {
   var msgArray = []; 
-  msgArray.push(`Hello, I'm ${bot.user.username} version ${pkg.version}, a Discord bot running on NodeJS Version 8.`);
-  msgArray.push(`My create date is January of 2017`)
+  msgArray.push(`Hello, I'm Zanity version 1.0, a Discord bot running on NodeJS Version 8, Lemonade API and HulkbotAPI.`);
+  msgArray.push(`My birthday is August 2018`)
   msgArray.push(`I'm currently running on Discord.js version 12.0.0 which utilizes the latest version of Discord.js, an *unofficial* Discord libary by hydrabolt.`);
   msgArray.push(`The developers of my source code are <@242734840829575169> and <@140487710727995392>.`);
   msgArray.push(`To see what I can do, use h!help`);
-  msgArray.push(`My invocation method is using prefixes, currently, I only respond to messages beginning with h!`);
+  msgArray.push(`My invocation method is using prefixes, currently, I only respond to messages beginning with -`);
   message.channel.send(msgArray)
 }
 
